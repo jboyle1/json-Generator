5bd9702358eef86e9584b8ef59cfa889465ff9f6
Jed Boyle <jedboyle01@gmail.com>
2020-07-09 11:09:23 +0100

    In main.js add query selectors to target #generate, #buttonContainer, and #displayContainer in index.html.

---
+++ static/js/main.js
@@ -0,0 +1,3 @@
+const jsonButton = document.querySelector('#generate');
+const buttonContainer = document.querySelector('#buttonContainer');
+const display = document.querySelector('#displayContainer');
