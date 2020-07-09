d593609080c5a077aaab54b87ebd88691e02abaa
Jed Boyle <jedboyle01@gmail.com>
2020-07-09 11:24:49 +0100

    Create a function called generateJson() that constructs a new XML request, checks that the response type is json, defines a function to be executed with the onreadystatechange property (this function will check if the XML HTTPRequest is 'DONE'. In turn  exicutes the (still to be made) renderResponse() and changeButton() functions. Lastly after the main block has finished exicuting, I use the open() method that takes 'GET' and the URL, and the send() method on xhr variable.

--- static/js/main.js
+++ static/js/main.js
@@ -3,3 +3,17 @@ const buttonContainer = document.querySelector('#buttonContainer');
 const display = document.querySelector('#displayContainer');
 
 const collection = ["Another", "More", "Next", "Continue", "Keep going", "Click Me", "A new one"];
+
+const generateJson = () => {
+    const xhr = new XMLHttpRequest();
+    xhr.responseType = 'json';
+
+    xhr.onreadystatechange = () => {
+        if (xhr.onreadystatechange === XMLHttpRequest.DONE) {
+            renderResponse(xhr.response);
+            changeButton();
+        };
+    };
+    xhr.open('GET', 'https://jsonplaceholder.typicode.com/users');
+    xhr.send();
+};
