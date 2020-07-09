34ffed9cb6fbdd800174cfd18c1cb2b5886d52e7
Jed Boyle <jedboyle01@gmail.com>
2020-07-09 11:11:37 +0100

    Add and array that holds the string values of the button.

--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,5 @@
 const jsonButton = document.querySelector('#generate');
 const buttonContainer = document.querySelector('#buttonContainer');
 const display = document.querySelector('#displayContainer');
+
+const collection = ["Another", "More", "Next", "Continue", "Keep going", "Click Me", "A new one"];
