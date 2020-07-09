db90d3e6f89a56c335459238b316cbac0e3625e5
Jed Boyle <jedboyle01@gmail.com>
2020-07-09 11:42:49 +0100

    Then add join(' ') to the bottom of the formatJson function.

--- static/js/main.js
+++ static/js/main.js
@@ -2,7 +2,15 @@ const jsonButton = document.querySelector('#generate');
 const buttonContainer = document.querySelector('#buttonContainer');
 const display = document.querySelector('#displayContainer');
 
-const collection = ["Another", "More", "Next", "Continue", "Keep going", "Click Me", "A new one"];
+const collection = [
+    "Another",
+    "More",
+    "Next",
+    "Continue",
+    "Keep going",
+    "Click Me",
+    "A new one"
+];
 
 const generateJson = () => {
     const xhr = new XMLHttpRequest();
@@ -21,18 +29,21 @@ const generateJson = () => {
 const formatJson = (resJson) => {
     resJson = JSON.stringify(resJson);
     let counter = 0;
-    return resJson.split('').map(char => {
-        switch (char) {
-            case ',':
-        return `,\n${' '.repeat(counter * 2)}`;
-      case '{':
-        counter += 1;
-        return `{\n${' '.repeat(counter * 2)}`;
-      case '}':
-        counter -= 1;
-        return `\n${' '.repeat(counter * 2)}}`;
-      default:
-        return char;
-        }
-    })
+    return resJson
+        .split('')
+        .map(char => {
+            switch (char) {
+                case ',':
+                    return `,\n${ ' '.repeat(counter * 2)}`;
+                case '{':
+                    counter += 1;
+                    return `{\n${ ' '.repeat(counter * 2)}`;
+                case '}':
+                    counter -= 1;
+                    return `\n${ ' '.repeat(counter * 2)}}`;
+                default:
+                    return char;
+            }
+        })
+        .join('');
 }
